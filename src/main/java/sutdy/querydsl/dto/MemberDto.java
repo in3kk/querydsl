9ada760af77a93fa949908898a750efca5721760
Jinse <akop4869@gmail.com>
2024-10-02 18:42:38 +0900

    day 7

--- src/main/java/sutdy/querydsl/dto/MemberDto.java
+++ src/main/java/sutdy/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package sutdy.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 
 @Data
@@ -7,6 +8,10 @@ public class MemberDto {
     private String username;
     private int age;
 
+    /**
+     * @QueryProjection 어노테이션을 이용하면 dto의 Q파일도 생성된다.
+     */
+    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
