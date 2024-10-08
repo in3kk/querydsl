f2be590ba8841a7379e22c3e40af3299389f1ac0
Jinse <akop4869@gmail.com>
2024-10-08 19:25:05 +0900

    day 9

---
+++ src/main/java/sutdy/querydsl/controller/MemberController.java
@@ -0,0 +1,21 @@
+package sutdy.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sutdy.querydsl.dto.MemberSearchCondition;
+import sutdy.querydsl.dto.MemberTeamDto;
+import sutdy.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
